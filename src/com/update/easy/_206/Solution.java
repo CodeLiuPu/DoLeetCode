f5a934204a3ae5aebbc711c19245ada880c46f43
liupu <575866447@qq.com>
2018-12-12 22:42:44 +0800

    206 链表反转

---
+++ src/com/update/easy/_206/Solution.java
@@ -0,0 +1,38 @@
+package com.update.easy._206;
+
+import com.update.ListNode;
+
+import javax.print.attribute.standard.NumberUp;
+
+/**
+ * 反转一个单链表。
+
+     示例:
+
+     输入: 1->2->3->4->5->NULL
+     输出: 5->4->3->2->1->NULL
+ */
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode now = head;
+
+        while (now != null){
+            ListNode next = now.next;
+            now.next = prev;
+            prev = now;
+            now = next;
+        }
+
+        return prev;
+    }
+
+}
