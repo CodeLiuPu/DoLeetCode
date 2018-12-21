70c0bca69560e5b4e7931220ddf5436a898a98e7
liupu <575866447@qq.com>
2018-12-21 23:57:14 +0800

    344 反转字符串 优化

---
+++ src/com/update/easy/_344/Solution2.java
@@ -0,0 +1,12 @@
+package com.update.easy._344;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+
+    }
+    public String reverseString(String s) {
+        return new StringBuffer(s).reverse().toString();
+    }
+
+}
