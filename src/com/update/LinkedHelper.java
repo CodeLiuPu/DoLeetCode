2d968b9ee42857f16c683803250644c2282a872a
liupu <575866447@qq.com>
2018-12-29 22:43:00 +0800

    Linked 工具类

---
+++ src/com/update/LinkedHelper.java
@@ -0,0 +1,33 @@
+package com.update;
+
+/**
+ * @author : liupu.
+ * @date : 2018/12/29
+ */
+public class LinkedHelper {
+
+    public static ListNode createLinkedList(int[] arr){
+        if (arr.length == 0){
+            return null;
+        }
+        ListNode head = new ListNode(arr[0]);
+        ListNode curr = head;
+
+        for (int i= 1;i<arr.length;i++){
+            ListNode node = new ListNode(arr[i]);
+            curr.next = node;
+            curr = node;
+        }
+        return head;
+    }
+
+    public static void  printLinkedList(ListNode head){
+        ListNode curr = head;
+        while (curr != null){
+
+            System.out.println("val = "+curr.val);
+            curr = curr.next;
+
+        }
+    }
+}
