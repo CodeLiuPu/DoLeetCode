15cfb45d482e62bc426add0116d2df71245f6a80
liupu <575866447@qq.com>
2018-12-31 10:20:43 +0800

    024 两两交换链表中的节点

---
+++ src/com/update/easy/_024/Solution.java
@@ -0,0 +1,42 @@
+package com.update.easy._024;
+
+import com.update.ListNode;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * <p>
+ * 说明:
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode node1 = prev.next;
+            ListNode node2 = prev.next.next;
+            ListNode next = node2.next;
+
+            node2.next = node1;
+            node1.next = next;
+            prev.next = node2;
+            prev = node1;
+
+        }
+        return dummyHead.next;
+    }
+}
