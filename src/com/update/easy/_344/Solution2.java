24ae808d27d0e902b9594155d43b486ea0412776
liupu <575866447@qq.com>
2019-01-04 13:15:04 +0800

    344 反转字符串

--- src/com/update/easy/_344/Solution2.java
+++ src/com/update/easy/_344/Solution2.java
@@ -5,8 +5,24 @@ public class Solution2 {
         Solution2 solution = new Solution2();
 
     }
+
     public String reverseString(String s) {
-        return new StringBuffer(s).reverse().toString();
+        int half = s.length() / 2 ;
+        int length = s.length();
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < half; i++) {
+            if (length - 1 - i >= 0){
+                swap(chars, i, length - 1 - i);
+            }
+        }
+        return new String(chars);
+    }
+
+    public void swap(char[] arrs, int x, int y) {
+        char temp = arrs[x];
+        arrs[x] = arrs[y];
+        arrs[y] = temp;
     }
 
 }
