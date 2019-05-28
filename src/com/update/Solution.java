7f5aa27bb6d49a78d5ee98c081a8b0c81247a381
liupu <575866447@qq.com>
2019-05-28 23:11:26 +0800

    Solution

--- src/com/update/Solution.java
+++ src/com/update/Solution.java
@@ -2,7 +2,7 @@ package com.update;
 
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+
 
     }
 }
--- src/com/update/easy/_151/Solution.java
+++ src/com/update/easy/_151/Solution.java
@@ -32,7 +32,7 @@ import java.util.List;
  */
 public class Solution {
     public static void main(String[] args) {
-        com.update.Solution solution = new com.update.Solution();
+        Solution solution = new Solution();
 
     }
 
